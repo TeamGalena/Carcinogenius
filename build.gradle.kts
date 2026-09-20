bb6a029c71d2d5918c2e1dd4631d3c1ef44ed46d
Niklas Widmann <nik@somethingcatchy.net>
2026-09-20 12:16:09 +0200

    update gradle setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,85 +1,18 @@
-import net.darkhax.curseforgegradle.Constants
-import net.darkhax.curseforgegradle.TaskPublishCurseForge
-import java.time.LocalDateTime
-
-val repository: String by extra
-val mod_name: String by extra
-val mod_author: String by extra
-val mod_id: String by extra
-val mod_version_suffix: String by extra
-val release_type: String by extra
-val modrinth_project_id: String by extra
-val curseforge_project_id: String by extra
-val minecraft_version: String by extra
-val parchment_version: String by extra
-val maven_group: String by extra
-val neoforge_version: String by extra
-val blueprint_version: String by extra
-val oreganized_version: String by extra
-val jade_version: String by extra
-val jei_version: String by extra
-val multikulti_version: String by extra
-
-val mod_version = System.getenv("RELEASE_VERSION")?.replace("-$mod_version_suffix", "")
-    ?: extra["mod_version"] as String
-
 plugins {
-    java
-    `maven-publish`
-    id("net.neoforged.gradle.userdev") version "7.0.184"
-    id("com.diffplug.spotless") version "7.0.4"
-    id("org.sonarqube") version "6.2.0.5505"
-    id("com.modrinth.minotaur") version "2.+"
-    id("net.darkhax.curseforgegradle") version "1.1.15"
+    id("com.possible-triangle.neoforge")
 }
 
-base {
-    archivesName = "$mod_name $minecraft_version-$mod_version"
-}
+// base {
+//     archivesName = "$mod_name $minecraft_version-$mod_version"
+// }
 
-java {
-    toolchain.languageVersion = JavaLanguageVersion.of(21)
-    withSourcesJar()
-}
-
-subsystems {
-    parchment {
-        minecraftVersion = minecraft_version
-        mappingsVersion = parchment_version
+neoforge {
+    dataGen {
+        existing("blueprint")
     }
-}
-
-runs {
-    create("client")
-
-    create("server") {
-        workingDirectory("run/server")
-    }
-
-    create("data") {
-        workingDirectory("run/data")
-
-        arguments(
-            "--mod",
-            mod_id,
-            "--all",
-            "--output",
-            file("src/generated/resources/").path,
-            "--existing",
-            file("src/main/resources/").path,
-            "--existing-mod",
-            "blueprint",
-        )
-    }
-}
-
-sourceSets.main {
-    resources.srcDir("src/generated/resources")
 }
 
 repositories {
-    mavenLocal()
-
     maven {
         url = uri("https://maven.teamabnormals.com/")
         content {
@@ -98,8 +31,7 @@ repositories {
             includeGroup("maven.modrinth")
         }
     }
-    maven {
-        url = uri("https://registry.somethingcatchy.net/repository/maven-public/")
+    nexus {
         content {
             includeGroup("dev.galena")
             includeGroup("com.possible-triangle")
@@ -107,139 +39,26 @@ repositories {
     }
 }
 
+val oreganized_version: String by extra
+val multikulti_version: String by extra
+val minecraft_version: String by extra
+val jei_version: String by extra
+val jade_version: String by extra
+
 dependencies {
-    implementation("net.neoforged:neoforge:${neoforge_version}")
-    implementation("com.teamabnormals:blueprint:${minecraft_version}-${blueprint_version}")
-    implementation("dev.galena:oreganized:${oreganized_version}")
-    runtimeOnly("com.possible-triangle:multikulti-datagen-neoforge:${minecraft_version}-${multikulti_version}")
+    modImplementation("dev.galena:oreganized:${oreganized_version}")
+    modRuntimeOnly("com.possible-triangle:multikulti-datagen-neoforge:${minecraft_version}-${multikulti_version}")
 
     // For dev testing
-    runtimeOnly("maven.modrinth:jade:${jade_version}")
+    modRuntimeOnly("maven.modrinth:jade:${jade_version}")
 
-    compileOnly("mezz.jei:jei-${minecraft_version}-common-api:${jei_version}")
-    compileOnly("mezz.jei:jei-${minecraft_version}-neoforge-api:${jei_version}")
-    runtimeOnly("mezz.jei:jei-${minecraft_version}-neoforge:${jei_version}")
+    modCompileOnly("mezz.jei:jei-${minecraft_version}-common-api:${jei_version}")
+    modCompileOnly("mezz.jei:jei-${minecraft_version}-neoforge-api:${jei_version}")
+    modRuntimeOnly("mezz.jei:jei-${minecraft_version}-neoforge:${jei_version}")
 }
 
-tasks.withType<Jar> {
-    val now = LocalDateTime.now().toString()
-    manifest {
-        attributes(
-            mapOf(
-                "Specification-Title" to mod_name,
-                "Specification-Vendor" to mod_author,
-                "Specification-Version" to mod_version,
-                "Implementation-Title" to mod_name,
-                "Implementation-Version" to mod_version,
-                "Implementation-Vendor" to mod_author,
-                "Implementation-Timestamp" to now,
-                "Built-On-Java" to "${System.getProperty("java.vm.version")} (${System.getProperty("java.vm.vendor")})",
-                "Build-On-Minecraft" to minecraft_version
-            )
-        )
-    }
-}
-
-tasks.withType<ProcessResources> {
-    filesMatching(
-        listOfNotNull(
-            "META-INF/mods.toml",
-            "META-INF/neoforge.mods.toml",
-            "pack.mcmeta",
-            "fabric.mod.json",
-            "${mod_id}*.mixins.json",
-        )
-    ) {
-        expand(
-            mapOf(
-                "mod_version" to mod_version,
-                "mod_name" to mod_name,
-                "mod_id" to mod_id,
-                "mod_author" to mod_author,
-                "repository" to repository,
-            )
-        )
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = maven_group
-            artifactId = mod_id.replace("_", "-")
-            version = mod_version
-
-            from(components["java"])
-
-            pom.withXml {
-                val node = asNode()
-                val list = node.get("dependencies") as groovy.util.NodeList
-                list.forEach { node.remove(it as groovy.util.Node) }
-            }
-        }
-    }
-    repositories {
-        mavenLocal()
-
-        val nexusToken = System.getenv("NEXUS_TOKEN")
-        val nexusUser = System.getenv("NEXUS_USER")
-        if (nexusToken != null && nexusUser != null) {
-            maven {
-                url = uri("https://registry.somethingcatchy.net/repository/maven-releases/")
-                credentials {
-                    username = nexusUser
-                    password = nexusToken
-                }
-            }
-        }
-    }
-}
-
-tasks.withType<GenerateModuleMetadata> {
-    enabled = false
-}
-
-spotless {
-    java {
-        importOrder()
-        removeUnusedImports()
-    }
-
-    kotlinGradle {
-        ktlint()
-        suppressLintsFor { shortCode = "standard:property-naming" }
-    }
-
-    json {
-        target("src/main/**/*.json")
-        gson().indentWithSpaces(2)
-    }
-}
-
-val upload = tasks.jar.get().archiveFile.get()
-
-modrinth {
-    projectId = modrinth_project_id
-    versionNumber = mod_version
-    versionName = "$mod_name $mod_version"
-    versionType = release_type
-    uploadFile = upload
-    gameVersions = listOf(minecraft_version)
-    changelog = System.getenv("CHANGELOG")
-    dependencies {
-        required.project("oreganized")
-    }
-}
-
-tasks.register<TaskPublishCurseForge>("curseforge") {
-    apiToken = System.getenv("CURSEFORGE_TOKEN")
-    upload(curseforge_project_id, upload) {
-        changelogType = Constants.CHANGELOG_MARKDOWN
-        changelog = System.getenv("CHANGELOG")
-        releaseType = release_type
-        displayName = "$mod_name $mod_version"
-        addGameVersion(minecraft_version)
-        addModLoader("NeoForge")
-        addRelation("oreganized", Constants.RELATION_REQUIRED)
+upload {
+    forEach {
+        dependencies.required("oreganized")
     }
 }
